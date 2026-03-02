4729df1af8a6a738318bf7b1b0314ce1bcad805a
Dundun2ee <rlawhdan363@gmail.com>
2026-03-02 14:46:39 +0900

    Title: 중앙값 구하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260302/중앙값 구하기/find-the-median.py
+++ 260302/중앙값 구하기/find-the-median.py
@@ -1,12 +1,14 @@
-arr = list(map(int, input().split()))
+a, b, c = map(int, input().split())
 
-if arr[0] > arr[1]:
-    if arr[0] > arr[2]:
-        arr[0], arr[2] = arr[2], arr[0]
-    else:
-        arr[0], arr[1] = arr[1], arr[0]
-else:
-    if arr[1] > arr[2]:
-        arr[1], arr[2] = arr[2], arr[1]
-
-print(arr[1])
+if a < b < c:
+    print(b)
+elif a < c < b:
+    print(c)
+elif b < a < c:
+    print(a)
+elif b < c < a:
+    print(c)
+elif c < a < b:
+    print(a)
+elif c < b < a:
+    print(b)
