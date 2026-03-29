d7639d5392587446504f5773332800da4eff0114
jongmu <rlawhdan363@gmail.com>
2026-03-29 19:36:04 +0900

    Title: 간단한 사칙연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260329/간단한 사칙연산/simple-arithmetic-operation.py
@@ -0,0 +1,14 @@
+# A, B 입력받기
+A, B = map(int, input().split())
+
+# 두 정수를 더헀을 때의 결과
+print(A + B)
+
+# A 에서 B를 뺐을 떄의 결과
+print(A - B)
+
+# A를 B로 나누었을 때의 몫
+print(A // B)
+
+# A를 B로 나누었을 때의 나머지
+print(A % B)
