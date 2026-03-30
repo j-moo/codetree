e0f7a4ae4833ba369059e7fa204c56c9f0abd840
jongmu <rlawhdan363@gmail.com>
2026-03-30 15:59:57 +0900

    Title: 시력 검사 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/시력 검사 2/eye-test-2.py
+++ 260330/시력 검사 2/eye-test-2.py
@@ -4,9 +4,9 @@ a = float(input())
 # 1.0 이상이면 High
 # 0.5 이상이면 Middle
 # 그 이외에는 Low 출력
-if a > 1.0:
+if a >= 1.0:
     print('High')
-elif a > 0.5:
+elif a >= 0.5:
     print('Middle')
 else:
     print('Low')
