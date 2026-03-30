9a9f1efeaa4d236090ba84a03813a205cbaada60
jongmu <rlawhdan363@gmail.com>
2026-03-30 15:59:46 +0900

    Title: 시력 검사 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260330/시력 검사 2/eye-test-2.py
@@ -0,0 +1,12 @@
+# 시력 입력받기(실수)
+a = float(input())
+
+# 1.0 이상이면 High
+# 0.5 이상이면 Middle
+# 그 이외에는 Low 출력
+if a > 1.0:
+    print('High')
+elif a > 0.5:
+    print('Middle')
+else:
+    print('Low')
