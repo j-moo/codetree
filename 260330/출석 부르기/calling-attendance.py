bfb0b64ecc2f1dc3d838e7f19023dc8ea32804ff
jongmu <rlawhdan363@gmail.com>
2026-03-30 16:06:32 +0900

    Title: 출석 부르기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/출석 부르기/calling-attendance.py
@@ -0,0 +1,12 @@
+# 출석번호 입력받기
+N = int(input())
+
+# 1번은 John, 2번은 Paul, 3번은 Vacancy
+if N == 1:
+    print('John')
+elif N == 2:
+    print('Tom')
+elif N == 3:
+    print('Paul')
+else:
+    print('Vacancy')
