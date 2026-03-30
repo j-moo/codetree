d9faf78bcb02803c047021de3337036df221f775
jongmu <rlawhdan363@gmail.com>
2026-03-30 16:17:27 +0900

    Title: 알파벳에 따른 평가, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+# 영문자 입력받기
+alpha = input()
+
+# 입력받은 문자에 따라 출력
+if alpha == 'S':
+    print('Superior')
+elif alpha == 'A':
+    print('Excellent')
+elif alpha == 'B':
+    print('Good')
+elif alpha == 'C':
+    print('Usually')
+elif alpha == 'D':
+    print('Effort')
+else:
+    print('Failure')
