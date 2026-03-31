2fa50a905e04df5e21bf8b2aecadb91568829016
jongmu <rlawhdan363@gmail.com>
2026-03-31 15:30:37 +0900

    Title: 남녀노소 구분짓기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260331/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,16 @@
+# 성별 입력받기 남자면 0, 여자는 1
+s = int(input())
+
+# 나이 입력받기
+a = int(input())
+
+if a >= 19:
+    if s == 1:
+        print('MAN')
+    else:
+        print('WOMAN')
+else:
+    if s == 1:
+        print('BOY')
+    else:
+        print('GIRL')
