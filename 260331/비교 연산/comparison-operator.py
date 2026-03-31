2d2a22ac2a90be13f0a81243aed1ca1081ab1548
jongmu <rlawhdan363@gmail.com>
2026-03-31 10:39:38 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260331/비교 연산/comparison-operator.py
@@ -0,0 +1,10 @@
+# 두 정수 입력받기
+A, B = map(int, input().split())
+
+# 조건들 판별하기
+print(1 if A >= B else 0)
+print(1 if A > B else 0)
+print(1 if A <= B else 0)
+print(1 if A < B else 0)
+print(1 if A == B else 0)
+print(1 if A != B else 0)
