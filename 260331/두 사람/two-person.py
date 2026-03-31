2258d6fa80e32e786666739b43db87e6290bf529
jongmu <rlawhdan363@gmail.com>
2026-03-31 14:44:14 +0900

    Title: 두 사람, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/두 사람/two-person.py
+++ 260331/두 사람/two-person.py
@@ -2,10 +2,9 @@
 a = input().split()
 b = input().split()
 
-if int(a[0]) >= 19 or int(b[0]) >= 19:
-    if a[1] == 'M' or b[1] == 'M':
-        print(1)
-    else:
-        print(0)
+if int(a[0]) >= 19 and a[1] == 'M':
+    print(1)
+elif int(b[0]) >= 19 and b[1] == 'M':
+    print(1)
 else:
     print(0)
