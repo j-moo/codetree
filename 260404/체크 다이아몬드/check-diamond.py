2cd46e29857917d18b0791f488b76c57c40380dc
jongmu <rlawhdan363@gmail.com>
2026-04-04 20:01:14 +0900

    Title: 체크 다이아몬드, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+space = N - 1 
+cnt = 1
+for i in range(2 * N - 1):
+    for j in range(space):
+        print(' ', end='')
+    
+    for k in range(cnt):
+        print('*', end=' ')
+    
+    if i < N - 1:
+        space -= 1
+        cnt += 1
+    else:
+        space += 1
+        cnt -= 1
+
+    print()
