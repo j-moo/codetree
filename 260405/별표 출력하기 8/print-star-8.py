fd183b12baa6aebdd8e1726d2f061b4a4ee7e3c8
jongmu <rlawhdan363@gmail.com>
2026-04-05 17:35:20 +0900

    Title: 별표 출력하기 8, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260405/별표 출력하기 8/print-star-8.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    if i % 2 == 1:
+        print('*', end=' ')
+    else:
+        for j in range(i):
+            print('*', end=' ')
+    
+    print()
