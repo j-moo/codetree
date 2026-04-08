391999cf16b6474609b6f10073449c85dfc780aa
jongmu <rlawhdan363@gmail.com>
2026-04-08 17:08:15 +0900

    Title: 좌우 지그재그 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/좌우 지그재그 출력/print-lr-zigzag.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+
+for i in range(N):
+    for j in range(N):
+        if i % 2 == 0:
+            print(N * i + 1 + j, end=' ')
+        else:
+            print(N * (i + 1) - j, end=' ')
+    print()
