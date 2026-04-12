a627834679384e302dcbc9e52b38a5154c907005
jongmu <rlawhdan363@gmail.com>
2026-04-12 15:02:24 +0900

    Title: 짝수의 합 n번 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260412/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(N):
+    a, b = map(int, input().split())
+
+    sum_val = 0
+    for j in range(a, b + 1):
+        if j % 2 == 0:
+            sum_val += j
+    
+    print(sum_val)
