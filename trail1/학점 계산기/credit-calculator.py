29062327bbae448147738997b979fa441a5a77be
jongmu <rlawhdan363@gmail.com>
2026-05-01 22:34:51 +0900

    [Passed] 학점 계산기 (45ms, 16MB)

--- trail1/학점 계산기/credit-calculator.py
+++ trail1/학점 계산기/credit-calculator.py
@@ -1,21 +1,21 @@
-N = int(input())
+# 과목의 개수 n을 입력받습니다.
+n = int(input())
+
+# 배열을 구현하여 주어진 수를 입력받습니다.
 arr = list(map(float, input().split()))
+	
+# 배열에 있는 실수들의 합을 구합니다.
+sum_val = sum(arr)
 
-sum_val = 0
-cnt = 0
-for elem in arr:
-    sum_val += elem
-    cnt += 1
+#평균 구하기
+avg = sum_val / n
 
-avg = sum_val / cnt
+# 출력
+print(f"{avg:.1f}")
 
-grade = ''
 if avg >= 4.0:
-    grade = 'Perfect'
+	print("Perfect")
 elif avg >= 3.0:
-    grade = 'Good'
+	print("Good")
 else:
-    grade = 'Poor'
-
-print(f'{avg:.1f}')
-print(grade)
+	print("Poor")
