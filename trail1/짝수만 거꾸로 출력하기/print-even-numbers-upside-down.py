2bfba3ef0726a1804e20e105c8aad0a127f7afb5
jongmu <rlawhdan363@gmail.com>
2026-05-01 23:02:30 +0900

    [Passed] 짝수만 거꾸로 출력하기 (45ms, 16MB)

--- trail1/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ trail1/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -1,10 +1,10 @@
-N = int(input())
+# 정수의 개수 n을 입력받습니다.
+n = int(input())
+
+# 배열을 구현하여 주어진 수를 입력받습니다.
 arr = list(map(int, input().split()))
-
-answer = []
-for elem in arr[::-1]:
-	if elem % 2 == 0:
-		answer.append(elem)
-
-
-print(*answer)	
+	
+# 입력받은 수들을 거꾸로 보면서 짝수인 값만 출력합니다.
+for i in range(n-1, -1, -1):
+    if arr[i] % 2 == 0:
+        print(arr[i], end=" ")
