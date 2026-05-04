56ecc5caefe0065d17720f7995864f45b44e6ade
jongmu <rlawhdan363@gmail.com>
2026-05-04 17:48:10 +0900

    [Passed] 2가 3번째로 등장하는 위치 (52ms, 16MB)

---
+++ trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,12 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+cnt = 0
+for i in range(N):
+    if arr[i] == 2:
+        cnt += 1
+    if cnt == 3:
+        answer = i + 1
+        break
+
+print(answer)
