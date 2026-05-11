27b9672184b86e8ee1a670e8bbb266b9f9fbca6d
jongmu <rlawhdan363@gmail.com>
2026-05-11 11:36:33 +0900

    [Passed] 격자 모양 출력하기 (46ms, 16MB)

---
+++ trail1/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+matrix = [[0] * N for _ in range(N)]
+
+for _ in range(M):
+    r, c = map(int, input().split())
+    matrix[r-1][c-1] = r * c
+
+for row in matrix:
+    for elem in row:
+        print(elem, end=' ')
+    print()
