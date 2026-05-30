6cb54b1e05ce45b51e7952d3984d7a9f3d04ccb4
jongmu <rlawhdan363@gmail.com>
2026-05-31 00:37:05 +0900

    [Passed] 짝수만 2로 나누기 (47ms, 16MB)

---
+++ trail2/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -0,0 +1,10 @@
+def modify(arr):
+    for i in range(len(arr)):
+        if arr[i] % 2 == 0:
+            arr[i] = arr[i] // 2
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+modify(arr)
+print(*arr)
