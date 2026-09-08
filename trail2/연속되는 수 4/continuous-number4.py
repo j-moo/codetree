bba51131d3f497fcde77f3390a053bc939f012a1
jongmu <rlawhdan363@gmail.com>
2026-09-08 20:01:17 +0900

    [Passed] 연속되는 수 4 (69ms, 17MB)

---
+++ trail2/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,18 @@
+n = int(input())
+prev = int(input())
+
+cnt = 1
+max_cnt = 1
+
+for i in range(n - 1):
+    cur = int(input())
+
+    if cur <= prev:
+        cnt = 1
+    else:
+        cnt += 1
+
+    prev = cur
+    max_cnt = max(cnt, max_cnt)
+
+print(max_cnt)
